b1cfd706bcc5aad10c6d2410bfcf1fc4918ad4a8
Michael Yin <layerssss@gmail.com>
2020-05-20 21:28:34 +1200

    remove github-actions

--- github-actions/push/README.md
+++
@@ -1,1 +0,0 @@
-# michaelhost-actions-push
--- github-actions/push/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl \
-  --fail \
-  -v -H "Authorization: Bearer ${MICHAELHOST_SECRET}" \
-  -X POST ${MICHAELHOST_WEBHOOK_URL}/image\?repoName\=${IMAGE_NAME}\&tag\=${IMAGE_TAG}
-
